f96a95803fd69e2a56fd21bc07353b7b784b6a10
Anand <phadnisanandhyd@gmail.com>
2024-10-20 14:26:13 +0530

    node push

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const blogRouter = require("./routes/BlogRoutes");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://phadnisanandhyd:<db_password>@cluster0.azvp2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 const app = express();
 
@@ -9,24 +12,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/blogs", blogRouter);
 
-//configure mongoose
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb+srv://phadnisanandhyd:M41BIvKhzzzelS9W@cluster0.azvp2.mongodb.net/Cluster0",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Connected to MongoDB");
-    }
-  }
-);
 
-app.listen(3001, () => {
-  console.log(`Server is running on port`);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 module.exports = app;
